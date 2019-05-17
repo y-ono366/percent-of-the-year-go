8a5d28f2e2dfe0c362b426309c29ee2e3b1f5326
yusuke ohno <yusuke@ohno.systems>
2019-05-17 15:34:17 +0900

    update

--- main.go
+++ main.go
@@ -3,30 +3,62 @@ package main
 import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/joho/godotenv"
 	"github.com/y-ono366/percent-of-the-year-go/src/common"
 	"github.com/y-ono366/percent-of-the-year-go/src/message"
+	"github.com/y-ono366/percent-of-the-year-go/src/twitter"
+	"log"
+	"os"
 )
+
+var Env string
+var Log *log.Logger
 
 type Response struct {
 	Message string `json:"message"`
 }
 
+func init() {
+	setEnvironment()
+	Log = common.LogInit()
+}
+
+func setEnvironment() {
+	switch os.Getenv("APP_ENV") {
+	case "production":
+		Env = "production"
+	default:
+		Env = "development"
+	}
+}
+
 func Handler() (Response, error) {
-	log := common.LogInit()
 	parcent, isFlg := message.GetParcent()
 	if isFlg == false {
-		log.Println("1%の進捗なし")
+		Log.Println("1%の進捗なし")
 		return Response{
 			Message: "1%の進捗なし",
 		}, nil
 	}
 	msg := message.CreateTweetMessage(parcent)
 	fmt.Println(msg)
+	client, err := twitter.GetTwClient()
+	if err != nil {
+	}
+	fmt.Println(client)
+
 	return Response{
 		Message: "完了",
 	}, nil
 }
 
 func main() {
-	lambda.Start(Handler)
+	if Env == "production" {
+		lambda.Start(Handler)
+	} else {
+		if err := godotenv.Load(); err != nil {
+			Log.Fatal(err)
+		}
+		Handler()
+	}
 }
